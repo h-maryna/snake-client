7e9ed510a9d2482285960dca6edcf9895a7214c2
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-12-01 20:13:05 +0000

    Added client.js and play.js updated

--- play.js
+++ play.js
@@ -1,25 +1,5 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on("connect", () =>{
-    console.log("Successfully connected");
-    conn.write("Name: Maryna")
-  })
-  conn.on("close", () => {
-    console.log("You ded cuz you idled");
-  })
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
+
 connect();
