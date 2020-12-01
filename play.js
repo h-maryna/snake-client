6e553da6292ef855b67935a5c4773685a326276b
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-12-01 19:16:00 +0000

    Play js added

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on("connect", () =>{
+    console.log("Successfully connected");
+    conn.write("Name: Maryna")
+  })
+  conn.on("close", () => {
+    console.log("You ded cuz you idled");
+  })
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
